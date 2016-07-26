de328140623b94c5e61e47f1e6fa43bf6eac7d0c
Evan Coleman <ecoleman@fnal.gov>
2016-07-26 03:28:02 -0500

    Clean up code for trial usage, implement small speed improvements

--- src/SelectorExample.C
+++ src/SelectorExample.C
@@ -1,11 +1,11 @@
-//root -l -q Delphes_Analysis/src/SelectorExample.C'("TChainArea/fileList.txt","out.root","Delphes")'
-
-//#include <iostream>
+//Usage: root -l -q Delphes_Analysis/src/SelectorExample.C'("TChainArea/fileList.txt","out.root","Delphes")'
 #include "../interface/BrownDelphesSelector.h"
 
-void SelectorExample(const char *inputFile, const char *outputFile, const char *treeName, const char *Selection="NoSelection"){
+void SelectorExample(const char *inputFile,           // absolute input path to the selector (.txt,.root)
+                     const char *outputFile,          // absolute path to output (.root) file 
+                     const char *treeName ="Delphes", // input tree to use
+                     const char *Selection="FCNC") {  // selection settings to use
     BrownDelphesSelector *Analysis = new BrownDelphesSelector;
     Analysis->Init(inputFile, outputFile, treeName, Selection);
     Analysis->Loop();
-    
 }
