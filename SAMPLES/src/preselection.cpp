0588c2fec2cacb51d749b8b66d0324aafe76705c
Soumya Ghosh <ghoshss93@gmail.com>
2016-09-10 06:48:42 -0500

    Comment out extraneous branch handlers
    Initially this was creating a bunch of branch handlers, many of them for
    branches we didn't need for the plot. Several of them did not have
    correct branch names and caused exceptions at runtime. They are now
    commented out and can be removed entirely.

--- SAMPLES/src/preselection.cpp
+++ SAMPLES/src/preselection.cpp
@@ -35,25 +35,25 @@ void preselection::analyze(size_t childid /* this info can be used for printouts
      * For skimmed ntuples, see below
      */
     d_ana::dBranchHandler<Electron>  elecs(tree(),"Electron");
-    d_ana::dBranchHandler<Muon>      muons(tree(),"Muon");
-    d_ana::dBranchHandler<MissingET> met(  tree(),"MET");
+    d_ana::dBranchHandler<Muon>      muons(tree(),"MuonTight");
+    //d_ana::dBranchHandler<MissingET> met(  tree(),"MissingET");
 
-    d_ana::dBranchHandler<Event>        events(      tree(),"Event");
-    d_ana::dBranchHandler<LHCOEvent>    lhcoevents(  tree(),"LHCOEvent");
-    d_ana::dBranchHandler<LHEFEvent>    lhefevents(  tree(),"LHEFEvent");
-    d_ana::dBranchHandler<LHEFWeight>   lhefweights( tree(),"LHEFWeight");
-    d_ana::dBranchHandler<HepMCEvent>   hepmcevents( tree(),"HepMCEvent");
-    d_ana::dBranchHandler<GenParticle>  genparticles(tree(),"GenParticle");
-    d_ana::dBranchHandler<Vertex>       vertices(    tree(),"Vertex");
-    d_ana::dBranchHandler<ScalarHT>     scalarht(    tree(),"ScalarHT");
-    d_ana::dBranchHandler<Rho>          rho(         tree(),"Rho");
-    d_ana::dBranchHandler<Weight>       weights(     tree(),"Weight");
-    d_ana::dBranchHandler<Photon>       photons(     tree(),"Photon");
+    //d_ana::dBranchHandler<Event>        events(      tree(),"Event");
+    //d_ana::dBranchHandler<LHCOEvent>    lhcoevents(  tree(),"LHCOEvent");
+    //d_ana::dBranchHandler<LHEFEvent>    lhefevents(  tree(),"LHEFEvent");
+    //d_ana::dBranchHandler<LHEFWeight>   lhefweights( tree(),"LHEFWeight");
+    //d_ana::dBranchHandler<HepMCEvent>   hepmcevents( tree(),"Event");
+    //d_ana::dBranchHandler<GenParticle>  genparticles(tree(),"GenParticle");
+    //d_ana::dBranchHandler<Vertex>       vertices(    tree(),"Vertex");
+    //d_ana::dBranchHandler<ScalarHT>     scalarht(    tree(),"ScalarHT");
+    //d_ana::dBranchHandler<Rho>          rho(         tree(),"Rho");
+    //d_ana::dBranchHandler<Weight>       weights(     tree(),"Weight");
+    //d_ana::dBranchHandler<Photon>       photons(     tree(),"Photon");
     d_ana::dBranchHandler<Jet>          jets(        tree(),"Jet");
-    d_ana::dBranchHandler<Track>        tracks(      tree(),"Track");
-    d_ana::dBranchHandler<Tower>        towers(      tree(),"Tower");
-    d_ana::dBranchHandler<HectorHit>    hectorhits(  tree(),"HectorHit");
-    d_ana::dBranchHandler<Candidate>    candidates(  tree(),"Candidate");
+    //d_ana::dBranchHandler<Track>        tracks(      tree(),"Track");
+    //d_ana::dBranchHandler<Tower>        towers(      tree(),"Tower");
+    //d_ana::dBranchHandler<HectorHit>    hectorhits(  tree(),"HectorHit");
+    //d_ana::dBranchHandler<Candidate>    candidates(  tree(),"Candidate");
 
     /* ==SKIM==
      *
@@ -112,41 +112,41 @@ void preselection::analyze(size_t childid /* this info can be used for printouts
     /*
      * Additional branches to store other Delphes objects
      */
-    std::vector<Event>       skimmedevents;  
-    std::vector<LHCOEvent>   skimmedlhcoevents;
-    std::vector<LHEFEvent>   skimmedlhefevents;
-    std::vector<LHEFWeight>  skimmedlhefweights;
-    std::vector<HepMCEvent>  skimmedhepmcevents;
-    std::vector<GenParticle> skimmedgenparticles;
-    std::vector<Vertex>      skimmedvertices;
-    std::vector<ScalarHT>    skimmedscalarht;
-    std::vector<Rho>         skimmedrho;
-    std::vector<Weight>      skimmedweights;
-    std::vector<Photon>      skimmedphotons;
+    //std::vector<Event>       skimmedevents;  
+    //std::vector<LHCOEvent>   skimmedlhcoevents;
+    //std::vector<LHEFEvent>   skimmedlhefevents;
+    //std::vector<LHEFWeight>  skimmedlhefweights;
+    //std::vector<HepMCEvent>  skimmedhepmcevents;
+    //std::vector<GenParticle> skimmedgenparticles;
+    //std::vector<Vertex>      skimmedvertices;
+    //std::vector<ScalarHT>    skimmedscalarht;
+    //std::vector<Rho>         skimmedrho;
+    //std::vector<Weight>      skimmedweights;
+    //std::vector<Photon>      skimmedphotons;
     std::vector<Jet>         skimmedjets;
-    std::vector<Track>       skimmedtracks;
-    std::vector<Tower>       skimmedtowers;
-    std::vector<HectorHit>   skimmedhectorhits;
-    std::vector<Candidate>   skimmedcandidates;
-    std::vector<MissingET>   skimmedmet;
+    //std::vector<Track>       skimmedtracks;
+    //std::vector<Tower>       skimmedtowers;
+    //std::vector<HectorHit>   skimmedhectorhits;
+    //std::vector<Candidate>   skimmedcandidates;
+    //std::vector<MissingET>   skimmedmet;
 
-    myskim->Branch("Event"        , &skimmedevents);
-    myskim->Branch("LHCOEvent"    , &skimmedlhcoevents);
-    myskim->Branch("LHEFEvent"    , &skimmedlhefevents);
-    myskim->Branch("LHEFWeight"   , &skimmedlhefweights);
-    myskim->Branch("HepMCEvent"   , &skimmedhepmcevents);
-    myskim->Branch("GenParticle"  , &skimmedgenparticles);
-    myskim->Branch("Vertex"       , &skimmedvertices);
-    myskim->Branch("ScalarHT"     , &skimmedscalarht);
-    myskim->Branch("Rho"          , &skimmedrho);
-    myskim->Branch("Weight"       , &skimmedweights);
-    myskim->Branch("Photon"       , &skimmedphotons);
+    //myskim->Branch("Event"        , &skimmedevents);
+    //myskim->Branch("LHCOEvent"    , &skimmedlhcoevents);
+    //myskim->Branch("LHEFEvent"    , &skimmedlhefevents);
+    //myskim->Branch("LHEFWeight"   , &skimmedlhefweights);
+    //myskim->Branch("Event"        , &skimmedhepmcevents);
+    //myskim->Branch("GenParticle"  , &skimmedgenparticles);
+    //myskim->Branch("Vertex"       , &skimmedvertices);
+    //myskim->Branch("ScalarHT"     , &skimmedscalarht);
+    //myskim->Branch("Rho"          , &skimmedrho);
+    //myskim->Branch("Weight"       , &skimmedweights);
+    //myskim->Branch("Photon"       , &skimmedphotons);
     myskim->Branch("Jet"          , &skimmedjets);
-    myskim->Branch("Track"        , &skimmedtracks);
-    myskim->Branch("Tower"        , &skimmedtowers);
-    myskim->Branch("HectorHit"    , &skimmedhectorhits);
-    myskim->Branch("Candidate"    , &skimmedcandidates);
-    myskim->Branch("MET"          , &skimmedmet);
+    //myskim->Branch("Track"        , &skimmedtracks);
+    //myskim->Branch("Tower"        , &skimmedtowers);
+    //myskim->Branch("HectorHit"    , &skimmedhectorhits);
+    //myskim->Branch("Candidate"    , &skimmedcandidates);
+    //myskim->Branch("MissingET"          , &skimmedmet);
 
 
 
@@ -218,41 +218,41 @@ void preselection::analyze(size_t childid /* this info can be used for printouts
          * Begin the event-by-event storage 
          */
 
-        skimmedevents.clear();  
-        skimmedlhcoevents.clear();
-        skimmedlhefevents.clear();
-        skimmedlhefweights.clear();
-        skimmedhepmcevents.clear();
-        skimmedgenparticles.clear();
-        skimmedvertices.clear();
-        skimmedscalarht.clear();
-        skimmedrho.clear();
-        skimmedweights.clear();
-        skimmedphotons.clear();
+        //skimmedevents.clear();  
+        //skimmedlhcoevents.clear();
+        //skimmedlhefevents.clear();
+        //skimmedlhefweights.clear();
+        //skimmedhepmcevents.clear();
+        //skimmedgenparticles.clear();
+        //skimmedvertices.clear();
+        //skimmedscalarht.clear();
+        //skimmedrho.clear();
+        //skimmedweights.clear();
+        //skimmedphotons.clear();
         skimmedjets.clear();
-        skimmedtracks.clear();
-        skimmedtowers.clear();
-        skimmedhectorhits.clear();
-        skimmedcandidates.clear();
-        skimmedmet.clear();
+        //skimmedtracks.clear();
+        //skimmedtowers.clear();
+        //skimmedhectorhits.clear();
+        //skimmedcandidates.clear();
+        //skimmedmet.clear();
 
-        for(size_t i=0; i<events.size(); i++)       skimmedevents.push_back(*events.at(i));
-        for(size_t i=0; i<lhcoevents.size(); i++)   skimmedlhcoevents.push_back(*lhcoevents.at(i));
-        for(size_t i=0; i<lhefevents.size(); i++)   skimmedlhefevents.push_back(*lhefevents.at(i));
-        for(size_t i=0; i<lhefweights.size(); i++)  skimmedlhefweights.push_back(*lhefweights.at(i));
-        for(size_t i=0; i<hepmcevents.size(); i++)  skimmedhepmcevents.push_back(*hepmcevents.at(i));
-        for(size_t i=0; i<genparticles.size(); i++) skimmedgenparticles.push_back(*genparticles.at(i));
-        for(size_t i=0; i<vertices.size(); i++)     skimmedvertices.push_back(*vertices.at(i));
-        for(size_t i=0; i<scalarht.size(); i++)     skimmedscalarht.push_back(*scalarht.at(i));
-        for(size_t i=0; i<rho.size(); i++)          skimmedrho.push_back(*rho.at(i));
-        for(size_t i=0; i<weights.size(); i++)      skimmedweights.push_back(*weights.at(i));
-        for(size_t i=0; i<photons.size(); i++)      skimmedphotons.push_back(*photons.at(i));
+        //for(size_t i=0; i<events.size(); i++)       skimmedevents.push_back(*events.at(i));
+        //for(size_t i=0; i<lhcoevents.size(); i++)   skimmedlhcoevents.push_back(*lhcoevents.at(i));
+        //for(size_t i=0; i<lhefevents.size(); i++)   skimmedlhefevents.push_back(*lhefevents.at(i));
+        //for(size_t i=0; i<lhefweights.size(); i++)  skimmedlhefweights.push_back(*lhefweights.at(i));
+        //for(size_t i=0; i<hepmcevents.size(); i++)  skimmedhepmcevents.push_back(*hepmcevents.at(i));
+        //for(size_t i=0; i<genparticles.size(); i++) skimmedgenparticles.push_back(*genparticles.at(i));
+        //for(size_t i=0; i<vertices.size(); i++)     skimmedvertices.push_back(*vertices.at(i));
+        //for(size_t i=0; i<scalarht.size(); i++)     skimmedscalarht.push_back(*scalarht.at(i));
+        //for(size_t i=0; i<rho.size(); i++)          skimmedrho.push_back(*rho.at(i));
+        //for(size_t i=0; i<weights.size(); i++)      skimmedweights.push_back(*weights.at(i));
+        //for(size_t i=0; i<photons.size(); i++)      skimmedphotons.push_back(*photons.at(i));
         for(size_t i=0; i<jets.size(); i++)         skimmedjets.push_back(*jets.at(i));
-        for(size_t i=0; i<tracks.size(); i++)       skimmedtracks.push_back(*tracks.at(i));
-        for(size_t i=0; i<towers.size(); i++)       skimmedtowers.push_back(*towers.at(i));
-        for(size_t i=0; i<hectorhits.size(); i++)   skimmedhectorhits.push_back(*hectorhits.at(i));
-        for(size_t i=0; i<candidates.size(); i++)   skimmedcandidates.push_back(*candidates.at(i));
-        for(size_t i=0; i<met.size(); i++)          skimmedmet.push_back(*met.at(i));
+        //for(size_t i=0; i<tracks.size(); i++)       skimmedtracks.push_back(*tracks.at(i));
+        //for(size_t i=0; i<towers.size(); i++)       skimmedtowers.push_back(*towers.at(i));
+        //for(size_t i=0; i<hectorhits.size(); i++)   skimmedhectorhits.push_back(*hectorhits.at(i));
+        //for(size_t i=0; i<candidates.size(); i++)   skimmedcandidates.push_back(*candidates.at(i));
+        //for(size_t i=0; i<met.size(); i++)          skimmedmet.push_back(*met.at(i));
 
         // Probably not the correct way to get nJets
         nJets = jets.size() - nTaus;
