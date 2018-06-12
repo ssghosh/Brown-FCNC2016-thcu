0dc4cd6d5f2e12da8650724ffee778133ee18dff
Soumya Ghosh <ghoshss93@gmail.com>
2018-06-12 16:09:31 -0500

    Change env.sh for an LPC installation of DAnalysis

--- SAMPLES/env.sh
+++ SAMPLES/env.sh
@@ -4,7 +4,8 @@ OLDDIR=`pwd`
 
 #DAnalysis
 #DANALYSISPATH=/afs/cern.ch/user/j/jkiesele/public/DAnalysis_framework/v.1.0.0_rc0-beta.2/DAnalysis
-DANALYSISPATH=/afs/cern.ch/user/j/jkiesele/public/DAnalysis_framework/v.1.1_rc2_special_d3.4/DAnalysis
+#DANALYSISPATH=/afs/cern.ch/user/j/jkiesele/public/DAnalysis_framework/v.1.1_rc2_special_d3.4/DAnalysis
+DANALYSISPATH=/uscms_data/d3/ssghosh/DAnalysis/DAnalysis
 
 
 #CMSSW and Delphes
@@ -25,5 +26,7 @@ export LD_LIBRARY_PATH=$PYTHIA8/lib:$LD_LIBRARY_PATH
 export DANALYSISPATH=$DANALYSISPATH
 export LD_LIBRARY_PATH=$DANALYSISPATH:$LD_LIBRARY_PATH
 export PATH=$PATH:$DANALYSISPATH
-export LD_LIBRARY_PATH=/afs/cern.ch/user/${USER: 0 : 1}/${USER}/Brown-FCNC2016-thcu/SAMPLES:$LD_LIBRARY_PATH
-export PATH=/afs/cern.ch/user/${USER: 0 : 1}/${USER}/Brown-FCNC2016-thcu/SAMPLES:$PATH
+#export LD_LIBRARY_PATH=/afs/cern.ch/user/${USER: 0 : 1}/${USER}/Brown-FCNC2016-thcu/SAMPLES:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/uscms_data/d3/${USER}/CMSSW_8_0_11/Brown-FCNC2016-thcu/SAMPLES:$LD_LIBRARY_PATH
+#export PATH=/afs/cern.ch/user/${USER: 0 : 1}/${USER}/Brown-FCNC2016-thcu/SAMPLES:$PATH
+export PATH=/uscms_data/d3/${USER}/CMSSW_8_0_11/src/Brown-FCNC2016-thcu/SAMPLES:$PATH
